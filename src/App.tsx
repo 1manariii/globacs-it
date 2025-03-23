5021353765e68fc0eb2101d28b10e9cac410f9b9
unknown <imanariii@vk.com>
2025-03-23 04:40:26 +0300

    Убрал из App неиспользуемый пакет

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Context from "./context/Context";
-
-const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
     <Context />
-  </QueryClientProvider>
   );
 }
 
